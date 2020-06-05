22d81a90474d343c266127a10046d3a3f306cf48
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-05 12:32:30 +0300

    Metacharacter Symbols

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 let re;
-re = /hello/i; //   i = case insensitive
-// re = /hello/g;  //  Global Search
+// Literal Characters
+re = /hello/;
+re = /hello/i;
 
-// console.log(re);
-// console.log(re.source);
+//  Metacharacter Symbols
+re = /^h/i;         //  Must start with
+re = / world$/i;    //  Must ends with
+re = /^hello$/i;    //  Must begin and end with 
+re = /h.llo/i;      //  Matches any ONE character
+re = /h*llo/i;      //  Matches any character 0 or more times
+re = /gre?a?y/i;    //  Optional character
+re = /gre?a?y\?/i;  //  Escape character
 
-// exec() - return result in an array or null
-// const result = re.exec('hello world');
 
-// console.log(result);
-// console.log(result[0]);
-// console.log(result.index);
-// console.log(result.input);
+//  String to match
+const str = 'Gry?';
 
-//  test() - Returns true or false
-// const result = re.test('Hello');
-// console.log(result);
+//  Log Results
+const result = re.exec(str);
+console.log(result);
 
-// match() - Return result array or null
-// const str = 'Hello There';
-// const result = str.match(re);
-// console.log(result);
+function reTest(re, str) {
+    if(re.test(str)) {
+        console.log(`${str} matched ${re.source}`);
+    } else {
+        console.log(`${str} does NOT match ${re.source}`);
+    }
+}
 
-//  search() - Returns index of the first match if not found returns - 1 
-// const str = 'Hello There';
-// const result = str.search(re);
-// console.log(result);
-
-// replace() - Return new strig with some or all matches of a patten
-const str = 'Hello There';
-const newStr = str.replace(re, 'Hi');
-console.log(newStr);
+reTest(re, str);
